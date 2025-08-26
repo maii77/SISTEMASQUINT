67c8d1d456121adf1c69c1e6062e0b41d1a69f75
Mai <maitemorales190708@gmail.com>
2025-08-26 09:11:54 -0300

    Tarea-Sistemas

---
+++ 02_ventanas/index.js
@@ -0,0 +1,28 @@
+
+console.log('==============================');
+console.log("Tarea02 - Ventanas con JavaScript");
+console.log('==============================');
+
+// Ejercicios con ventanas
+
+// 1.Creamos una variable 
+let numeroUno = prompt('Intr')
+
+//2. imprimo la variable en la consola
+console.log(numeroUno)
+
+//3. Otra variable para guardar numeros 
+let numeroDos=prompt('Introduce otro numero')
+
+//4. Imprimimos de nuevo el otro numero 
+console.log(numeroDos);
+
+//Importante: convertir a numeros los datos 
+numeroUno=parseInt(numeroUno)
+numeroDos=parseInt(numeroDos)
+
+//5. Creamos una variable para los resultados
+let resultado=numeroUno + numeroDos
+
+//6. Imprimimos el resultado
+console.log(resultado);
